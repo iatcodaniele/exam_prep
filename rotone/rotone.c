10bdef40e95d59a72854ed09d245897d6c10b343
Daniele <iatcodaniele@gmail.com>
2023-10-23 17:03:28 +0200

    Add files via upload

---
+++ rotone/rotone.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void rotone(char *str)
+{
+    int i =0;
+
+    while(str[i])
+    {
+        if((str[i] >= 'a' && str[i] <= 'y') || (str[i] >= 'A' && str[i] <= 'Y'))
+        {   
+            ft_putchar(str[i]+1);
+        }
+        else if(str[i] == 'z' || str[i] == 'Z')
+        {
+            ft_putchar(str[i] - 25);
+        }
+        else
+        {
+            ft_putchar(str[i]);
+        } 
+        i++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if(argc ==  2)
+    {
+        rotone(argv[1]);
+        ft_putchar('\n');
+    }
+    ft_putchar('\n');
+    return 0;
+}
