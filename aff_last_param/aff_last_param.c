7f8a424cb673c40d8f6c0a861beaa07d5e49a374
Daniele <iatcodaniele@gmail.com>
2023-10-26 16:48:20 +0200

    Update aff_last_param.c

--- aff_last_param/aff_last_param.c
+++ aff_last_param/aff_last_param.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
     if(argc > 1)
     {
-        while(*argv[argc-1])
+        while(*argv[argc-1]) //argument count - 1 = last argument
         {
             write(1, (argv[argc-1]++), 1);
         }
