9c83bf7b3cda2ebd79226804f649005cdab34e18
Daniele <iatcodaniele@gmail.com>
2023-10-26 18:27:52 +0200

    Update iter(long).c

--- iter/iter(long).c
+++ iter/iter(long).c
@@ -7,8 +7,8 @@ void ft_putchar(char c)
     write(1, &c, 1);
 }
 
-int check(char *str, char c, int index)
-{
+int check(char *str, char c, int index) //checks whether a character has been found before
+{                                        
     int i = 0;
 
     while(str[i] != '\0' && i < index)
@@ -27,20 +27,20 @@ void inter(char *str1, char *str2)
     
     while(str1[i] != '\0')
     {
-        if(check(str1, str1[i], i) == 0)
-        {
+        if(check(str1, str1[i], i) == 0) //if current character isn't a duplicate, enter
+        {                                //if character if a duplicate, move along i++
             j = 0;
             while(str2[j] != '\0')
             {
-                if(str2[j] == str1[i])
-                {
+                if(str2[j] == str1[i]) //if char in s2 is the same as s1, display and exit
+                {                       
                     ft_putchar(str1[i]);
-                    break;
+                    break;    //using break to avoid printing all occurences of s1 char in s2
                 }
                 j++;
             }
         }
-        i++;
+        i++; 
     }
 }
 
