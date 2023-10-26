3ce0ff2f54b402eac4034989f31e85be0331ef9b
Daniele <iatcodaniele@gmail.com>
2023-10-26 18:06:23 +0200

    Update ft_atoi.c

--- atoi/ft_atoi.c
+++ atoi/ft_atoi.c
@@ -1,54 +1,48 @@
-//atoi function
-
-int ft_is_space(char *str)
+int is_space(char c)
 {
-    int i = 0;
-
-    if(str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\r' || str[i] == '\f' || str[i] == 32)
+    if(c == ' ' || c == '\n' || c == '\t' || c == '\r' || c == '\v' || c == '\f' || c == '\b')
     {
-        return (1);
+        return 1;
     }
-    return (0);
+    return 0;
 }
 
 int ft_atoi(char *str)
 {
-    int res;
-    int i;
-    int sign;
-    
-    res = 0;
-    i = 0;
-    sign = 1;
-    
-    while(ft_is_space(str))
-        	i++;
-    	
-    while(str[i] == '-' || str[i] == '+')
+    int i = 0;
+    int sign = 1;
+    int res = 0;
+
+    while(is_space(str[i]) == 1) //skips spaces
+    {
+        i++;
+    }
+       
+    while(str[i] == '-' || str[i] == '+') //handles negative numbers
     {
         if(str[i++] == '-')
-        	sign*= -1;
+        {
+            sign *= -1;
+        }
     }
-    while(str[i] != '\0')
+    while(str[i]!= '\0')
     {
         if(str[i] >= '0' && str[i] <= '9')
         {
             res = res * 10;
-            res = res + (str[i++] - '0');
+            res = res + (str[i++] - 48); //res gets filled with the converted numbers
         }
         else
-            return (res * sign);
+            return (res * sign); //number * sign to determine if its a positive or negative number
     }
     return (res * sign);
 }
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    printf("%d\n", ft_atoi("-42"));
-    printf("%d\n", ft_atoi("42"));
-    printf("%d\n", ft_atoi("2147483647"));
-    printf("%d\n", ft_atoi("-2147483648"));
-    printf("%d\n", ft_atoi("    42 42"));
+    printf("%d\n", atoi("   42   42")); //doesn't compile
+    printf("%d", ft_atoi("   42"));
 }
