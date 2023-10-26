0c8cacadcd56dc91f72e4c170eee0a9e5cf08afd
Daniele <iatcodaniele@gmail.com>
2023-10-26 18:35:17 +0200

    Update putnbr.c

--- putnbr/putnbr.c
+++ putnbr/putnbr.c
@@ -2,9 +2,9 @@
 
 void    ft_putnbr(int nb)
 {
-    long nbl = nb;
+    long nbl = nb; //handles integer limits
     int i = 0;
-    char c[10];
+    char c[10]; //max integer has 10 digits
 
     if(nbl == 0)
     {
@@ -17,15 +17,15 @@ void    ft_putnbr(int nb)
     }
     while(nbl > 0)
     {
-        c[i] = (nbl % 10) + 48;
-        nbl = nbl / 10;
-        i++;
+        c[i] = (nbl % 10) + 48; //storing the number backwards by doing modulo
+        nbl = nbl / 10; //discarding the number we just stored
+        i++;//iterates until and including null
     }
-    i--;
+    i--; //going back -1 to re-enter string
     while( i >= 0)
     {
-        write(1, &c[i], 1);
-        i--;
+        write(1, &c[i], 1); //printing the string backwards
+        i--;                //so that the original number is displayed correctly
     }
 }
 
