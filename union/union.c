f2321c0d6ced5a3afcd9ced464813d303e4d340a
Daniele <iatcodaniele@gmail.com>
2023-10-27 12:31:03 +0200

    Update union.c

--- union/union.c
+++ union/union.c
@@ -18,14 +18,14 @@ int check_doubles(char *str, char c, int index)
     return 0;
 }
 
-void inter2(char *str1, char *str2)
+void union(char *str1, char *str2)
 {
     int i = 0;
     int j = 0;
 
     while(str1[i] != '\0')
     {
-        if(check_doubles(str1, str1[i], i) == 0)
+        if(check_doubles(str1, str1[i], i) == 0) //print first string while there are no doubles
         {
             ft_putchar(str1[i]);
             
@@ -34,9 +34,9 @@ void inter2(char *str1, char *str2)
     }
     while(str2[j] != '\0')
     {
-        if(check_doubles(str1, str2[j], i) == 0 && check_doubles(str2, str2[j], j) == 0)
+        if(check_doubles(str1, str2[j], i) == 0 && check_doubles(str2, str2[j], j) == 0) 
         {
-            ft_putchar(str2[j]);
+            ft_putchar(str2[j]); //print second string while there are no doubles from both first and second string
         }
         j++;
     }
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
 {
     if(argc == 3)
     {
-        inter2(argv[1], argv[2]);
+        union(argv[1], argv[2]);
     }
     ft_putchar('\n');
     return 0;
