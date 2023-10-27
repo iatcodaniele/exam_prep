984823ea4583963a89ffbca2b0acc599951b3d65
Daniele <iatcodaniele@gmail.com>
2023-10-27 12:37:49 +0200

    Update iter.c

--- iter/iter.c
+++ iter/iter.c
@@ -3,18 +3,18 @@ int	main(int argc, char **argv)
 {
 	int	i = 0;
 	int	j;
-	int	tab[256] = {0};
+	int	tab[256] = {0}; //array of all ASCII characters, initialized with zeros
 	if (argc == 3)
 	while (argv[1][i])
 	{
 		j = 0;
 		while (argv[2][j])
 		{
-			if (argv[1][i] == argv[2][j])
+			if (argv[1][i] == argv[2][j]) //if a match is found
 			{
-				if (tab[(int)argv[2][j]] == 0)
+				if (tab[(int)argv[2][j]] == 0) //if the match has not been found before
 				{
-					tab[(int)argv[2][j]] = 1;
+					tab[(int)argv[2][j]] = 1; //set it to 1 = found
 					write (1, &argv[1][i], 1);
 				}
 			}
