1a4333ef37d732aa090f5745479a6b930a2491af
Daniele <iatcodaniele@gmail.com>
2023-10-27 12:58:53 +0200

    Update ft_strrev.c

--- ft_strrev/ft_strrev.c
+++ ft_strrev/ft_strrev.c
@@ -15,21 +15,22 @@ char *ft_strrev(char *str)
     int end = ft_strlen(str) - 1; //-1 to exclude the null byte
     char temp;
 
-    while(start < ((end + 1) / 2)) 
-    {
+    while(str[start] < str[end]) //swap function to invert the position
+    {                            //of the first elements to that of the last
            temp = str[start];
-           str[start] = str[end-start];
-           str[end - start] = temp;
+           str[start] = str[end];
+           str[end] = temp;
            start++;
-           
+           end--;
     }
     return (str);
 }
-
+//an even number of letters in a word leads to it not swapping the middle ones
 #include <stdio.h>
 int main()
 {
-    char x[] = "start";
-
+    char x[] = "start"; 
+    //the function is a pointer so you can't just have printf("string") = segfault
+    //always have a separate variable
     printf("%s", ft_strrev(x));
 }
