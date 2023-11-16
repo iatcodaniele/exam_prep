ded9356d52514b29a04a1a605d4c7e305d720d29
Daniele <iatcodaniele@gmail.com>
2023-11-16 12:47:31 +0100

    Create ft_atoi(exam_specific).c

---
+++ atoi/ft_atoi(exam_specific).c
@@ -0,0 +1,36 @@
+
+int ft_is_space(char c)
+{
+	if(c == ' ' || c == '\b' || c == '\t' || c == '\n' 
+			|| c == '\v' || c == '\f' || c == '\r')
+		return 1;
+	return 0;
+}
+
+int ft_atoi(const char *str)
+{
+	int i = 0;
+	int res = 0;
+	int sign = 1;
+
+	while(ft_is_space(str[i]) ==  1)
+	{
+		i++;
+	}
+	if(str[i] == '-' || str[i] == '+')
+	{
+		if(str[i++] == '-')
+			sign *= -1;
+	}
+	while(str[i] !='\0')
+	{	
+		if(str[i] >= '0' && str[i] <= '9')
+		{
+			res = res * 10;
+			res = res + (str[i++] - '0');
+		}
+		else
+			return res * sign;
+	}
+	return res * sign;
+}
