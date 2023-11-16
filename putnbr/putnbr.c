ca5ccbb2320c6416fdf9853ce056e183465cb3f1
Daniele <iatcodaniele@gmail.com>
2023-11-16 13:20:22 +0100

    Update putnbr.c

--- putnbr/putnbr.c
+++ putnbr/putnbr.c
@@ -1,35 +1,42 @@
 #include <unistd.h>
+#include <stdio.h>
 
-void    ft_putnbr(int nb)
+void ft_putchar(char c)
 {
-    long nbl = nb; //handles integer limits
-    int i = 0;
-    char c[10]; //max integer has 10 digits
-
-    if(nbl == 0)
-    {
-        write(1, "0", 1);
-    }
-    else if(nbl < 0)
-    {
-        write(1, "-", 1);
-        nbl = -nbl;
-    }
-    while(nbl > 0)
-    {
-        c[i] = (nbl % 10) + 48; //storing the number backwards by doing modulo
-        nbl = nbl / 10; //discarding the number we just stored
-        i++;//iterates until and including null
-    }
-    i--; //going back -1 to re-enter string
-    while( i >= 0)
-    {
-        write(1, &c[i], 1); //printing the string backwards
-        i--;                //so that the original number is displayed correctly
-    }
+	write(1, &c, 1);
 }
 
-#include <stdio.h>
+void ft_putnbr(int nb)
+{
+	int i = 0;
+	char c[10];
+	long nbl = nb;
+	
+	if(nbl < 0)
+	{
+		ft_putchar('-');
+		nbl *=-1;
+	}
+	
+	if(nbl == 0)
+	{
+		ft_putchar('0');
+	}
+
+	while(nbl > 0)
+	{
+		c[i] = (nbl % 10) + 48;
+		nbl /=10;
+		i++;
+	}
+	i--;
+	while(i >= 0)
+	{
+		ft_putchar(c[i]);
+		i--;
+	}
+}
+
 int main()
 {
     ft_putnbr(42);
