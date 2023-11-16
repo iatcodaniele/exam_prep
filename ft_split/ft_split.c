cb05aa8dc43ceae31bd869f9ff28ffdabffca67b
Daniele <iatcodaniele@gmail.com>
2023-11-16 12:57:20 +0100

    Update ft_split.c

--- ft_split/ft_split.c
+++ ft_split/ft_split.c
@@ -9,6 +9,17 @@ int ft_space(char c)
 	return 0;
 }
 
+int ft_strlen(char *str)
+{
+	int i = 0;
+
+	while(str[i])
+	{	
+		i++;
+	}
+	return i;
+}
+	
 int count_words(char *str)
 {
 	int counter = 0;
@@ -27,11 +38,10 @@ int count_words(char *str)
 char *word_malloc(char *str) //allocating memory for individual words
 {
 	int i = 0;
+	int size = ft_strlen(str);
 	char *word;
-
-	while(str[i] && ft_space(str[i]) == 1) //if there are white spaces at the beggining of string
-		i++; //skip them
-	word = (char*)malloc(sizeof(char) * (i + 1));
+	
+	word = (char*)malloc(sizeof(char) * (size + 1));
 	if(!word)
 	{
 		return NULL;
@@ -62,7 +72,7 @@ char **ft_split(char *str)
 		while(*str && ft_space(*str) == 1) //skip spaces when it finds them
 			str++; 
 		if(*str && ft_space(*str) == 0) //if only printables, enter condition
-		{
+		{ //word_malloc starts storing from the first letter so no need to check spaces again
 			array[i] = word_malloc(str); //store word allocation inside array allocation
 			i++;
 			while(*str && ft_space(*str) == 0) //move forwards inside string while there are no spaces(word delimiters)
@@ -85,15 +95,13 @@ int main()
 		printf("better luck next time");
 		return 1;
 	}
-
+	
 	int i = 0;
 	while(arr[i] != NULL)
 	{
 		printf("%s\n", arr[i]);
-		free(arr[i]);
 		i++;
 	}
-	free(arr); //freeing the block of memory when we're done using it(to prevent memory leaks)
 	return 0;
 }
 
