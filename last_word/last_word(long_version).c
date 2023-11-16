be24f74295cd772994d98b0e9ce86c3e03acac26
Daniele <iatcodaniele@gmail.com>
2023-11-16 13:16:22 +0100

    Update and rename last_word.c to last_word(long_version).c

---
+++ last_word/last_word(long_version).c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+/*void last_word(char *str)
+{
+    int i = 0;
+    int j;
+
+    while(str[i])
+    {
+        if(str[i] == ' ' && str[i+1] >= 33 && str[i+1] <= 126)
+        {
+            j = i+1;
+        }
+        i++;
+    }
+    while(str[j] >= 33 && str[j] <= 126)
+    {
+        ft_putchar(str[j]);
+        j++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if(argc == 2)
+    {
+        last_word(argv[1]);
+        ft_putchar('\n');
+    }
+    ft_putchar('\n');
+}*/
+
+
+
+
