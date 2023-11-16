f847a79dad8ac031d6729eb0bebb9cc426979db3
Daniele <iatcodaniele@gmail.com>
2023-11-16 13:32:00 +0100

    Update ulstr.c

--- ulstr/ulstr.c
+++ ulstr/ulstr.c
@@ -4,7 +4,7 @@ void ft_putchar(char c)
 {
     write(1, &c, 1);
 }
-/*
+
 void ulstr(char *str)
 {
     int i = 0;
@@ -37,31 +37,5 @@ int main(int argc, char **argv)
         ft_putchar('\n');
     }
     ft_putchar('\n');
-    return 0;
-}
-*/
-
-int main(int argc, char **argv)
-{
-    if(argc == 2)
-    {
-        while(*argv[1])
-        {
-            if(*argv[1] >= 'a' && *argv[1] <= 'z')
-            {
-                *argv[1] -= 32;   //if its lowercase, make uppercase
-                ft_putchar(*argv[1]);
-            }
-            else if(*argv[1] >= 'A' && *argv[1] <= 'Z')
-            {
-                *argv[1] += 32;  //if its uppercase, make lowercase
-                ft_putchar(*argv[1]);
-            }
-            else
-            	ft_putchar(*argv[1]); //if any other character, print as it is
-            argv[1]++;
-        }
-    }
-    write(1, "\n", 1);
     return 0;
 }
