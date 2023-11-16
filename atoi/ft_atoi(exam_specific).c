4b24afa9671c81d72d4664135f2fc36cbea440d4
Daniele <iatcodaniele@gmail.com>
2023-11-16 13:26:27 +0100

    Update ft_atoi(exam_specific).c

--- atoi/ft_atoi(exam_specific).c
+++ atoi/ft_atoi(exam_specific).c
@@ -13,24 +13,28 @@ int ft_atoi(const char *str)
 	int res = 0;
 	int sign = 1;
 
-	while(ft_is_space(str[i]) ==  1)
-	{
-		i++;
-	}
-	if(str[i] == '-' || str[i] == '+')
-	{
-		if(str[i++] == '-')
-			sign *= -1;
-	}
 	while(str[i] !='\0')
-	{	
+	{
+		while(ft_is_space(str[i] == 1))
+			i++;
+		
+		if(str[i] == '-' || str[i] == '+')
+		{
+			if(str[i++] == '-')
+				sign *=-1;
+		}	
+		
 		if(str[i] >= '0' && str[i] <= '9')
 		{
 			res = res * 10;
 			res = res + (str[i++] - '0');
 		}
+		else if(ft_is_space(str[i]) == 1)
+		
+			i++;
 		else
-			return res * sign;
+			return res * sign;	
 	}
+
 	return res * sign;
 }
